bf1a77c90c70d8e359486cc66a7e9c3bd4cf8742
endricklosange <endricklosange@gmail.com>
2022-12-16 09:14:00 +0100

    (optionel) style de la page de recherche

--- search.php
+++ search.php
@@ -8,32 +8,58 @@ if (!empty($_POST) && isset($_POST['search'])) {
     $allSeries = $b->search($_POST['title']);
 }
 ?>
+
 <!DOCTYPE html>
 
 <html>
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <?php require_once('head.php') ?>
     <title>series</title>
 </head>
-<body>
-    <?php require_once('navbar.php'); ?>
-    <div class="row">
-    <h1>Series</h1>
 
-        <?php foreach ($allSeries as $series) { ?>
-            <div class="col-md-4">
-                <div class="card" style="width: 18rem;">
-                    <img src="..." class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title"><?= $series->getTitle() ?></h5>
+<body>
+    <div class="row firstRow">
+        <div class="col-md-2">
+            <?php require_once('navbar.php'); ?>
+        </div>
+        <div class="col-md-10">
+
+            <div class="row">
+                <h1>Series</h1>
+
+                <?php foreach ($allSeries as $series) { ?>
+                    <?php
+                    $b = new Books($series->getId());
+                    if (isset($b->all($series->getId())[0])) {
+                        $book = $b->all($series->getId());
+                    } else {
+                    }
+                    ?>
+                    <div class="col-md-3">
+
+                        <div class="customCard">
+                            <div class="row">
+                                <div class="col-md-4 ">
+                                    <?php if (isset($b->all($series->getId())[0])) { ?>
+                                        <img src="/assets/uploads/<?= $book[0]->getCover(); ?>" class="card-img-top" alt="...">
+                                    <?php } else { ?>
+                                        <img src="/assets/img/no-image.jpg" class="card-img-top" alt="...">
+                                    <?php } ?>
+                                </div>
+                                <div class="col-md-8">
+                                    <h2 class="card-title"><?= $series->getTitle() ?> </h2>
+                                    <h3 class="card-title"><?= $series->getOrigin() ?> </h3>
+                                    <?php if (isset($b->all($series->getId())[0])) { ?>
+                                        <h4 class="card-title">Scénario : <?= $book[0]->getWriter() ?> </h4>
+                                    <?php } ?>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                </div>
+                <?php } ?>
             </div>
-        <?php } ?>
+        </div>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 </body>
